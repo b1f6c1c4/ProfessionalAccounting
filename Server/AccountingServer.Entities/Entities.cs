ed4419ba4a6ef5b5a5728db48b05dbceaa866e73
b1f6c1c4 <tujinzheng@126.com>
2014-11-30 15:22:07 +0800

    添加Entities的注释

--- Server/AccountingServer.Entities/Entities.cs
+++ Server/AccountingServer.Entities/Entities.cs
@@ -123,12 +123,30 @@ namespace AccountingServer.Entities
     //    public IAssetItem[] Schedule { get; set; }
     //}
 
+    /// <summary>
+    /// 余额表项目
+    /// </summary>
     public class Balance
     {
+        /// <summary>
+        /// 日期
+        /// </summary>
         public DateTime? Date { get; set; }
+        /// <summary>
+        /// 一级科目编号
+        /// </summary>
         public int? Title { get; set; }
+        /// <summary>
+        /// 二级科目编号
+        /// </summary>
         public int? SubTitle { get; set; }
+        /// <summary>
+        /// 内容
+        /// </summary>
         public string Content { get; set; }
+        /// <summary>
+        /// 余额
+        /// </summary>
         public double Fund { get; set; }
     }
 }
