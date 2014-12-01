bc0de1a5671af851ebca0bba649feb96a34c12ec
b1f6c1c4 <tujinzheng@126.com>
2014-12-01 18:56:47 +0800

    从Accountant中分离TitleManager

--- Server/AccountingServer.BLL/Accountant.cs
+++ Server/AccountingServer.BLL/Accountant.cs
@@ -55,42 +55,6 @@ namespace AccountingServer.BLL
         }
 
         /// <summary>
-        ///     返回编号对应的会计科目名称
-        /// </summary>
-        /// <param name="title">一级科目编号</param>
-        /// <param name="subtitle">二级科目编号</param>
-        /// <returns>名称</returns>
-        public static string GetTitleName(int? title, int? subtitle = null)
-        {
-            var mgr = new ResourceManager("AccountingServer.BLL.AccountTitle", Assembly.GetExecutingAssembly());
-            return mgr.GetString(String.Format("T{0:0000}{1:00}", title, subtitle));
-        }
-
-        /// <summary>
-        ///     返回细目对应的会计科目名称
-        /// </summary>
-        /// <param name="detail">细目</param>
-        /// <returns>名称</returns>
-        public static string GetTitleName(VoucherDetail detail)
-        {
-            return detail.SubTitle.HasValue
-                       ? GetTitleName(detail.Title) + "-" + GetTitleName(detail.Title, detail.SubTitle)
-                       : GetTitleName(detail.Title);
-        }
-
-        /// <summary>
-        ///     返回余额对应的会计科目名称
-        /// </summary>
-        /// <param name="balance">余额</param>
-        /// <returns>名称</returns>
-        public static string GetTitleName(Balance balance)
-        {
-            return balance.SubTitle.HasValue
-                       ? GetTitleName(balance.Title) + "-" + GetTitleName(balance.Title, balance.SubTitle)
-                       : GetTitleName(balance.Title);
-        }
-
-        /// <summary>
         ///     检查记账凭证借贷方数额是否相等
         /// </summary>
         /// <param name="entity">记账凭证</param>
---
+++ Server/AccountingServer.BLL/TitleManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using AccountingServer.Entities;
+
+namespace AccountingServer.BLL
+{
+    public static class TitleManager
+    {
+        /// <summary>
+        ///     返回编号对应的会计科目名称
+        /// </summary>
+        /// <param name="title">一级科目编号</param>
+        /// <param name="subtitle">二级科目编号</param>
+        /// <returns>名称</returns>
+        public static string GetTitleName(int? title, int? subtitle = null)
+        {
+            var mgr = new ResourceManager("AccountingServer.BLL.AccountTitle", Assembly.GetExecutingAssembly());
+            return mgr.GetString(String.Format("T{0:0000}{1:00}", title, subtitle));
+        }
+
+        /// <summary>
+        ///     返回细目对应的会计科目名称
+        /// </summary>
+        /// <param name="detail">细目</param>
+        /// <returns>名称</returns>
+        public static string GetTitleName(VoucherDetail detail)
+        {
+            return detail.SubTitle.HasValue
+                       ? GetTitleName(detail.Title) + "-" + GetTitleName(detail.Title, detail.SubTitle)
+                       : GetTitleName(detail.Title);
+        }
+
+        /// <summary>
+        ///     返回余额对应的会计科目名称
+        /// </summary>
+        /// <param name="balance">余额</param>
+        /// <returns>名称</returns>
+        public static string GetTitleName(Balance balance)
+        {
+            return balance.SubTitle.HasValue
+                       ? GetTitleName(balance.Title) + "-" + GetTitleName(balance.Title, balance.SubTitle)
+                       : GetTitleName(balance.Title);
+        }
+    }
+}
