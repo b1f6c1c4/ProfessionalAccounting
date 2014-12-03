57dadf079b1b843ea315c81113b83710bf0e71b1
b1f6c1c4 <tujinzheng@126.com>
2014-12-03 14:49:18 +0800

    测试

---
+++ Server/AccountingServer.DAL.UnitTest/AccountantTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccountingServer.BLL;
+using AccountingServer.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AccountingServer.DAL.UnitTest
+{
+    [TestClass]
+    public class AccountantTest
+    {
+        //private readonly Accountant m_Accountant;
+
+        //public AccountantTest() { m_Accountant = new Accountant(); }
+
+        [TestMethod]
+        public void ProcessDaily()
+        {
+            var res = Accountant.ProcessDailyBalance(
+                                                     new[]
+                                                         {
+                                                             new Balance { Date = null, Fund = 1D },
+                                                             new Balance { Date = DateTime.Parse("2014-01-01"), Fund = 10D },
+                                                             new Balance { Date = DateTime.Parse("2014-01-02"), Fund = 100D },
+                                                             new Balance { Date = DateTime.Parse("2014-01-04"), Fund = 1000D },
+                                                             new Balance { Date = DateTime.Parse("2014-01-05"), Fund = 10000D },
+                                                         },
+                                                     null,
+                                                     null).ToList();
+            var exp = new[]
+                          {
+                              new Balance { Date = DateTime.Parse("2014-01-01"), Fund = 11D },
+                              new Balance { Date = DateTime.Parse("2014-01-02"), Fund = 111D },
+                              new Balance { Date = DateTime.Parse("2014-01-03"), Fund = 111D },
+                              new Balance { Date = DateTime.Parse("2014-01-04"), Fund = 1111D },
+                              new Balance { Date = DateTime.Parse("2014-01-05"), Fund = 11111D },
+                          };
+
+            Assert.IsTrue(res.SequenceEqual(exp, new BalanceEqualityComparer()));
+        }
+    }
+}
