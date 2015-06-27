a8a09502e4589e849689cb6b18495579136ed843
b1f6c1c4 <tujinzheng@126.com>
2015-06-27 23:56:52 +0800

    补PluginBase的注释

--- Server/AccountingServer.Console/Plugin/PluginBase.cs
+++ Server/AccountingServer.Console/Plugin/PluginBase.cs
@@ -2,6 +2,9 @@
 
 namespace AccountingServer.Console.Plugin
 {
+    /// <summary>
+    ///     插件基类
+    /// </summary>
     public abstract class PluginBase
     {
         /// <summary>
@@ -11,6 +14,11 @@ namespace AccountingServer.Console.Plugin
 
         protected PluginBase(Accountant accountant) { Accountant = accountant; }
 
+        /// <summary>
+        ///     执行插件表达式
+        /// </summary>
+        /// <param name="pars">参数</param>
+        /// <returns>执行结果</returns>
         public abstract IQueryResult Execute(params string[] pars);
     }
 }
