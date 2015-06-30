69b3bd7c209b3b3f16e0b27935bb13beaf750d5f
b1f6c1c4 <tujinzheng@126.com>
2015-06-30 08:42:37 +0800

    拆THUInfo的长函数

--- Server/AccountingServer.Plugins.THUInfo/THUInfo.Process.cs
+++ Server/AccountingServer.Plugins.THUInfo/THUInfo.Process.cs
@@ -4,8 +4,8 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using AccountingServer.BLL;
-using AccountingServer.Shell;
 using AccountingServer.Entities;
+using AccountingServer.Shell;
 
 namespace AccountingServer.Plugins.THUInfo
 {
@@ -121,170 +121,205 @@ namespace AccountingServer.Plugins.THUInfo
         /// <param name="records">交易记录</param>
         /// <param name="pars">生成记账所需参数</param>
         /// <param name="failed">无法生产记账凭证的交易记录</param>
-        private IEnumerable<Voucher> AutoGenerate(IEnumerable<TransactionRecord> records, IEnumerable<string> pars,
-                                                  out List<TransactionRecord> failed)
+        private static IEnumerable<Voucher> AutoGenerate(IEnumerable<TransactionRecord> records,
+                                                         IEnumerable<string> pars,
+                                                         out List<TransactionRecord> failed)
         {
             var dic = GetDic(pars);
-            var res = new List<Voucher>();
+            var res = Enumerable.Empty<Voucher>();
             failed = new List<TransactionRecord>();
             foreach (var grp in records.GroupBy(r => r.Date))
             {
-                var lst = new List<TransactionRecord>();
-                foreach (var record in grp)
-                {
-                    var rec = record;
-                    Func<int, VoucherDetail> item =
-                        dir => new VoucherDetail
-                                   {
-                                       Title = 1012,
-                                       SubTitle = 05,
-                                       Fund = dir * rec.Fund,
-                                       Remark = rec.Index.ToString(CultureInfo.InvariantCulture)
-                                   };
-                    switch (record.Type)
-                    {
-                        case "支付宝充值":
-                            res.Add(
-                                    new Voucher
-                                        {
-                                            Date = grp.Key.Date,
-                                            Details = new List<VoucherDetail>
-                                                          {
-                                                              item(1),
-                                                              new VoucherDetail
-                                                                  {
-                                                                      Title = 1221,
-                                                                      Content = "学生卡待领",
-                                                                      Fund = -record.Fund
-                                                                  }
-                                                          }
-                                        });
-                            break;
-                        case "领取圈存":
-                            res.Add(
-                                    new Voucher
-                                        {
-                                            Date = grp.Key.Date,
-                                            Details = new List<VoucherDetail>
-                                                          {
-                                                              item(1),
-                                                              new VoucherDetail
-                                                                  {
-                                                                      Title = 1002,
-                                                                      Content = "3593",
-                                                                      Fund = -record.Fund
-                                                                  }
-                                                          }
-                                        });
-                            break;
-                        case "自助缴费(学生公寓水费)":
-                            res.Add(
-                                    new Voucher
-                                        {
-                                            Date = grp.Key.Date,
-                                            Details = new List<VoucherDetail>
-                                                          {
-                                                              item(-1),
-                                                              new VoucherDetail
-                                                                  {
-                                                                      Title = 1123,
-                                                                      Content = "学生卡小钱包",
-                                                                      Fund = record.Fund
-                                                                  }
-                                                          }
-                                        });
-                            break;
-                        default:
-                            lst.Add(record);
-                            break;
-                    }
-                }
+                List<Voucher> res0, res1;
+                List<TransactionRecord> lst;
+                GenerateCommon(grp, out res0, out lst);
+                res = res.Concat(res0);
                 if (!lst.Any())
                     continue;
 
                 if (!dic.ContainsKey(grp.Key.Date))
-                    failed.AddRange(grp);
-
-                var ins = dic[grp.Key.Date];
-                var id = 0;
-                foreach (var inst in ins)
                 {
-                    if (id == lst.Count)
-                        throw new ArgumentException("生成记账所需参数不足", "pars");
-                    Func<TransactionRecord, int, VoucherDetail> newDetail =
-                        (tr, dir) => new VoucherDetail
-                                         {
-                                             Title = 1012,
-                                             SubTitle = 05,
-                                             Fund = dir * tr.Fund,
-                                             Remark = tr.Index.ToString(CultureInfo.InvariantCulture)
-                                         };
-                    switch (inst.Item1)
-                    {
-                        case RegularType.Meals:
-                            DateTime? t = null;
-                            while (true)
-                            {
-                                if (t.HasValue)
-                                {
-                                    if (lst[id].Time.Subtract(t.Value).TotalHours > 1)
-                                        break;
-                                }
-                                else
-                                    t = lst[id].Time;
-                                if (lst[id].Type == "消费" &&
-                                    lst[id].Location == "饮食中心")
-                                    res.Add(
-                                            new Voucher
-                                                {
-                                                    Date = grp.Key.Date,
-                                                    Details = new List<VoucherDetail>
-                                                                  {
-                                                                      newDetail(lst[id], -1),
-                                                                      new VoucherDetail
-                                                                          {
-                                                                              Title = 6602,
-                                                                              SubTitle = 03,
-                                                                              Content = inst.Item2,
-                                                                              Fund = lst[id].Fund
-                                                                          }
-                                                                  }
-                                                });
-                                else
-                                    break;
-                                id++;
-                                if (id == lst.Count)
-                                    break;
-                            }
-                            break;
-                        case RegularType.Shopping:
-                            if (lst[id].Type == "消费" &&
-                                (lst[id].Location == "紫荆服务楼超市" ||
-                                 lst[id].Location == "饮食广场超市"))
-                            {
-                                res.Add(
-                                        new Voucher
-                                            {
-                                                Date = grp.Key.Date,
-                                                Details = new List<VoucherDetail>
-                                                              {
-                                                                  newDetail(lst[id], -1),
-                                                                  new VoucherDetail
-                                                                      {
-                                                                          Title = 6602,
-                                                                          SubTitle = 03,
-                                                                          Content = inst.Item2,
-                                                                          Fund = lst[id].Fund
-                                                                      }
-                                                              }
-                                            });
-                                id++;
-                            }
-                            break;
-                    }
+                    failed.AddRange(grp);
+                    continue;
                 }
+
+                GenerateSpecial(dic[grp.Key.Date], lst, grp.Key.Date, out res1);
+                res = res.Concat(res1);
             }
             return res;
+        }
+
+        /// <summary>
+        ///     自动生成常规记账凭证
+        /// </summary>
+        /// <param name="recordsGroup">待处理交易记录组</param>
+        /// <param name="result">生成的记账凭证</param>
+        /// <param name="specialRecords">未能处理的交易记录</param>
+        private static void GenerateCommon(IGrouping<DateTime, TransactionRecord> recordsGroup, out List<Voucher> result,
+                                           out List<TransactionRecord> specialRecords)
+        {
+            specialRecords = new List<TransactionRecord>();
+            result = new List<Voucher>();
+            foreach (var record in recordsGroup)
+            {
+                var rec = record;
+                Func<int, VoucherDetail> item =
+                    dir => new VoucherDetail
+                               {
+                                   Title = 1012,
+                                   SubTitle = 05,
+                                   Fund = dir * rec.Fund,
+                                   Remark = rec.Index.ToString(CultureInfo.InvariantCulture)
+                               };
+                switch (record.Type)
+                {
+                    case "支付宝充值":
+                        result.Add(
+                                   new Voucher
+                                       {
+                                           Date = recordsGroup.Key.Date,
+                                           Details = new List<VoucherDetail>
+                                                         {
+                                                             item(1),
+                                                             new VoucherDetail
+                                                                 {
+                                                                     Title = 1221,
+                                                                     Content = "学生卡待领",
+                                                                     Fund = -record.Fund
+                                                                 }
+                                                         }
+                                       });
+                        break;
+                    case "领取圈存":
+                        result.Add(
+                                   new Voucher
+                                       {
+                                           Date = recordsGroup.Key.Date,
+                                           Details = new List<VoucherDetail>
+                                                         {
+                                                             item(1),
+                                                             new VoucherDetail
+                                                                 {
+                                                                     Title = 1002,
+                                                                     Content = "3593",
+                                                                     Fund = -record.Fund
+                                                                 }
+                                                         }
+                                       });
+                        break;
+                    case "自助缴费(学生公寓水费)":
+                        result.Add(
+                                   new Voucher
+                                       {
+                                           Date = recordsGroup.Key.Date,
+                                           Details = new List<VoucherDetail>
+                                                         {
+                                                             item(-1),
+                                                             new VoucherDetail
+                                                                 {
+                                                                     Title = 1123,
+                                                                     Content = "学生卡小钱包",
+                                                                     Fund = record.Fund
+                                                                 }
+                                                         }
+                                       });
+                        break;
+                    default:
+                        specialRecords.Add(record);
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        ///     自动生成常规记账凭证
+        /// </summary>
+        /// <param name="par">参数</param>
+        /// <param name="records">待处理的交易记录</param>
+        /// <param name="date">日期</param>
+        /// <param name="result">生成的记账凭证</param>
+        private static void GenerateSpecial(IEnumerable<Tuple<RegularType, string>> par,
+                                            IReadOnlyList<TransactionRecord> records, DateTime date,
+                                            out List<Voucher> result)
+        {
+            result = new List<Voucher>();
+            var id = 0;
+            foreach (var inst in par)
+            {
+                if (id == records.Count)
+                    throw new ArgumentException("生成记账所需参数不足", "par");
+                Func<TransactionRecord, int, VoucherDetail> newDetail =
+                    (tr, dir) => new VoucherDetail
+                                     {
+                                         Title = 1012,
+                                         SubTitle = 05,
+                                         Fund = dir * tr.Fund,
+                                         Remark = tr.Index.ToString(CultureInfo.InvariantCulture)
+                                     };
+                switch (inst.Item1)
+                {
+                    case RegularType.Meals:
+                        DateTime? t = null;
+                        while (true)
+                        {
+                            if (t.HasValue)
+                            {
+                                if (records[id].Time.Subtract(t.Value).TotalHours > 1)
+                                    break;
+                            }
+                            else
+                                t = records[id].Time;
+                            if (records[id].Type == "消费" &&
+                                records[id].Location == "饮食中心")
+                                result.Add(
+                                           new Voucher
+                                               {
+                                                   Date = date,
+                                                   Details = new List<VoucherDetail>
+                                                                 {
+                                                                     newDetail(records[id], -1),
+                                                                     new VoucherDetail
+                                                                         {
+                                                                             Title = 6602,
+                                                                             SubTitle = 03,
+                                                                             Content = inst.Item2,
+                                                                             Fund = records[id].Fund
+                                                                         }
+                                                                 }
+                                               });
+                            else
+                                break;
+                            id++;
+                            if (id == records.Count)
+                                break;
+                        }
+                        break;
+                    case RegularType.Shopping:
+                        if (records[id].Type == "消费" &&
+                            (records[id].Location == "紫荆服务楼超市" ||
+                             records[id].Location == "饮食广场超市"))
+                        {
+                            result.Add(
+                                       new Voucher
+                                           {
+                                               Date = date,
+                                               Details = new List<VoucherDetail>
+                                                             {
+                                                                 newDetail(records[id], -1),
+                                                                 new VoucherDetail
+                                                                     {
+                                                                         Title = 6602,
+                                                                         SubTitle = 03,
+                                                                         Content = inst.Item2,
+                                                                         Fund = records[id].Fund
+                                                                     }
+                                                             }
+                                           });
+                            id++;
+                        }
+                        break;
+                }
+            }
         }
 
         /// <summary>
