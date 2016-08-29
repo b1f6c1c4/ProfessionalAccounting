ab44561d8802d1b72e0e369211d80fe4c8ecb584
b1f6c1c4 <b1f6c1c4@gmail.com>
2016-08-29 13:31:43 +0800

    endpoint to xml

---
+++ AccountingServer.Plugins.THUInfo/EndPointTemplate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace AccountingServer.Plugins.THUInfo
+{
+    [Serializable]
+    internal class EndPointTemplates
+    {
+        [XmlElement("Template")] public List<EndPointTemplate> Templates;
+    }
+
+    [Serializable]
+    internal class EndPointTemplate
+    {
+        public string Name { get; set; }
+
+        [XmlArrayItem("Range")] public List<EndPointRange> Ranges;
+    }
+
+    [Serializable]
+    internal class EndPointRange
+    {
+        public int Start { get; set; }
+
+        public int End { get; set; }
+    }
+}
