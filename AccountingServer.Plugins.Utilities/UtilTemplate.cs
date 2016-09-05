f35a8ffbd170cb5f03ea1b0949700e6a50b1ea86
b1f6c1c4 <b1f6c1c4@gmail.com>
2016-09-05 14:39:24 +0800

    fix namespace

--- AccountingServer.Plugins.Utilities/UtilTemplate.cs
+++ AccountingServer.Plugins.Utilities/UtilTemplate.cs
@@ -3,9 +3,7 @@ using System.Collections.Generic;
 using System.Xml.Serialization;
 using AccountingServer.Entities;
 
-// ReSharper disable once CheckNamespace
-
-namespace AccountingServer.Plugins
+namespace AccountingServer.Plugins.Utilities
 {
     [Serializable]
     [XmlRoot("Templates")]
