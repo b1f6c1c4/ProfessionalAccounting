d689101035f1e93a4ee7f2b362852ca06295321b
b1f6c1c4 <b1f6c1c4@gmail.com>
2017-01-06 10:50:23 +0800

    add comments

--- AccountingServer.BLL/CustomManager.cs
+++ AccountingServer.BLL/CustomManager.cs
@@ -4,14 +4,31 @@ using System.Xml.Serialization;
 
 namespace AccountingServer.BLL
 {
+    /// <summary>
+    ///     配置文件管理器
+    /// </summary>
+    /// <typeparam name="T">配置文件类型</typeparam>
     public class CustomManager<T>
     {
+        /// <summary>
+        ///     文件名
+        /// </summary>
         private readonly string m_FileName;
 
+        /// <summary>
+        ///     配置文件
+        /// </summary>
         private readonly T m_Config;
 
+        /// <summary>
+        ///     读取配置文件时发生的错误
+        /// </summary>
         private readonly Exception m_Exception;
 
+        /// <summary>
+        ///     读取配置文件；若发生错误，保存在<c>m_Exception</c>中
+        /// </summary>
+        /// <param name="filename">文件名</param>
         public CustomManager(string filename)
         {
             m_FileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, filename);
@@ -28,6 +45,9 @@ namespace AccountingServer.BLL
             }
         }
 
+        /// <summary>
+        ///     配置文件
+        /// </summary>
         public T Config
         {
             get
