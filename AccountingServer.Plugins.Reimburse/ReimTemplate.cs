e6dc79f0f594c956e252c89f48b46751d42b567b
b1f6c1c4 <b1f6c1c4@gmail.com>
2017-02-01 10:52:24 +0800

    reimburse

---
+++ AccountingServer.Plugins.Reimburse/ReimTemplate.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Xml.Serialization;
+using AccountingServer.Entities;
+
+namespace AccountingServer.Plugins.Reimburse
+{
+    [Serializable]
+    [XmlRoot("Templates")]
+    public class ReimTemplates
+    {
+        /// <summary>
+        ///     财月分隔
+        /// </summary>
+        [XmlAttribute("day")]
+        public int Day { get; set; }
+
+        [XmlElement("Template")] public List<ReimTemplate> Templates;
+    }
+
+    [Serializable]
+    public class ReimTemplate : Voucher
+    {
+        [XmlAttribute("name")]
+        public string Name { get; set; }
+
+        [XmlText]
+        public string Query { get; set; }
+
+        [XmlAttribute("ratio")]
+        public double Ratio { get; set; }
+
+        [XmlAttribute("ext")]
+        [DefaultValue(false)]
+        public bool IsLeftExtended { get; set; }
+
+        [XmlAttribute("byTitle")]
+        [DefaultValue(false)]
+        public bool ByTitle { get; set; }
+
+        [XmlAttribute("bySubTitle")]
+        [DefaultValue(false)]
+        public bool BySubTitle { get; set; }
+
+        [XmlAttribute("byContent")]
+        [DefaultValue(false)]
+        public bool ByCountent { get; set; }
+
+        [XmlAttribute("hideContent")]
+        [DefaultValue(false)]
+        public bool HideCountent { get; set; }
+
+        [XmlAttribute("byRemark")]
+        [DefaultValue(false)]
+        public bool ByRemark { get; set; }
+    }
+}
---
+++ AccountingServer.Plugins.Reimburse/Reimburse.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Text;
+using AccountingServer.BLL;
+using AccountingServer.BLL.Parsing;
+using AccountingServer.Entities;
+using AccountingServer.Shell;
+using AccountingServer.Shell.Carry;
+using static AccountingServer.BLL.Parsing.FacadeF;
+
+namespace AccountingServer.Plugins.Reimburse
+{
+    /// <summary>
+    ///     报销
+    /// </summary>
+    public class Reimburse : PluginBase
+    {
+        private static readonly ConfigManager<ReimTemplates> Templates =
+            new ConfigManager<ReimTemplates>("Reim.xml");
+
+        public Reimburse(Accountant accountant) : base(accountant) { }
+
+        /// <inheritdoc />
+        public override IQueryResult Execute(string expr)
+        {
+            DateFilter the;
+            if (string.IsNullOrWhiteSpace(expr))
+            {
+                var now = DateTime.Today;
+                if (now.Day > Templates.Config.Day)
+                    the = new DateFilter(
+                        new DateTime(now.Year, now.Month, Templates.Config.Day + 1),
+                        new DateTime(now.Year, now.Month, Templates.Config.Day).AddMonths(1));
+                else
+                    the = new DateFilter(
+                        new DateTime(now.Year, now.Month, Templates.Config.Day + 1).AddMonths(-1),
+                        new DateTime(now.Year, now.Month, Templates.Config.Day));
+            }
+            else
+            {
+                var rng = ParsingF.Range(ref expr);
+                if (!rng.HasValue)
+                    throw new ArgumentException("语法错误", nameof(expr));
+
+                ParsingF.Eof(expr);
+                the = rng.Value;
+            }
+
+            return DoReimbursement(the);
+        }
+
+        /// <inheritdoc />
+        public override string ListHelp()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine(base.ListHelp());
+            foreach (var reim in Templates.Config.Templates)
+                sb.AppendLine(
+                    $"{reim.Name}\t\t{reim.Query} {(reim.IsLeftExtended ? "[~X]" : "[X]")} `{(reim.ByTitle ? "t" : "")}{(reim.BySubTitle ? "s" : "")}{(reim.ByCountent ? "c" : "")}{(reim.ByRemark ? "r" : "")}");
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        ///     执行报销
+        /// </summary>
+        /// <param name="rng">财月</param>
+        /// <returns>执行结果</returns>
+        private IQueryResult DoReimbursement(DateFilter rng)
+        {
+            var sb = new StringBuilder();
+            var f = ExchangeFactory.Create();
+            foreach (var reim in Templates.Config.Templates)
+            {
+                var gq = Accountant
+                    .RunGroupedQuery(
+                        $"{reim.Query} [{(reim.IsLeftExtended ? "" : rng.StartDate.AsDate())}~{rng.EndDate.AsDate()}] G `C{(reim.ByTitle ? "t" : "")}{(reim.BySubTitle ? "s" : "")}{(reim.ByCountent ? "c" : "")}{(reim.ByRemark ? "r" : "")}");
+                foreach (var grp in gq.GroupByCurrency())
+                {
+                    var curr = grp.Key;
+                    // ReSharper disable once PossibleInvalidOperationException
+                    var ratio = curr == Voucher.BaseCurrency ? 1 : f.From(rng.EndDate.Value, curr);
+                    foreach (var b in grp)
+                    {
+                        sb.Append(reim.Name);
+                        if (reim.ByTitle)
+                            sb.Append($"/{TitleManager.GetTitleName(b.Title)}");
+                        if (reim.BySubTitle)
+                            sb.Append($"/{TitleManager.GetTitleName(b.Title, b.SubTitle)}");
+                        if (reim.ByCountent && !reim.HideCountent)
+                            sb.Append($"/{b.Content}");
+                        if (reim.ByRemark)
+                            sb.Append($"/{b.Remark}");
+                        sb.AppendLine($"\t{curr}\t{b.Fund}\t{ratio}\t{reim.Ratio}");
+                    }
+                }
+            }
+
+            return new UnEditableText(sb.ToString());
+        }
+    }
+}
