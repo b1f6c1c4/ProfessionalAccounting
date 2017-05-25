e562c3380a30c00897198327e8e4191a85eb0dd5
b1f6c1c4 <b1f6c1c4@gmail.com>
2017-05-25 21:43:40 +0800

    unit test using xunit

---
+++ AccountingServer.Entities.Test/AccountantHelperTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using AccountingServer.Entities.Util;
+using Xunit;
+
+namespace AccountingServer.Entities.Test
+{
+    [SuppressMessage("ReSharper", "InvokeAsExtensionMethod")]
+    public class AccountantHelperTest
+    {
+        [Theory]
+        [InlineData("2017-01-31", 2017, 1)]
+        [InlineData("2017-02-28", 2017, 2)]
+        [InlineData("2017-04-30", 2017, 4)]
+        [InlineData("2020-01-31", 2020, 1)]
+        [InlineData("2020-02-29", 2020, 2)]
+        [InlineData("2020-04-30", 2020, 4)]
+        [InlineData("2100-01-31", 2100, 1)]
+        [InlineData("2100-02-28", 2100, 2)]
+        [InlineData("2100-04-30", 2100, 4)]
+        public void LastDayOfMonthTest(string expectedS, int year, int month)
+        {
+            var expected = DateHelperTest.DateTimeFromString(expectedS);
+            Assert.Equal(expected, AccountantHelper.LastDayOfMonth(year, month));
+        }
+    }
+}
---
+++ AccountingServer.Entities.Test/DateHelperTest.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Xunit;
+
+namespace AccountingServer.Entities.Test
+{
+    [SuppressMessage("ReSharper", "InvokeAsExtensionMethod")]
+    public class DateHelperTest
+    {
+        public static DateTime? DateTimeFromString(string b1S) => b1S == null ? (DateTime?)null : DateTime.Parse(b1S);
+
+        [Theory]
+        [InlineData(0, null, null)]
+        [InlineData(+1, "2017-01-01", null)]
+        [InlineData(-1, "2016-12-31", "2017-01-01")]
+        [InlineData(0, "2017-01-01", "2017-01-01")]
+        [InlineData(+1, "2017-01-02", "2017-01-01")]
+        public void CompareDateTest(int expected, string b1S, string b2S)
+        {
+            var b1 = DateTimeFromString(b1S);
+            var b2 = DateTimeFromString(b2S);
+
+            var result = DateHelper.CompareDate(b1, b2);
+            if (expected == 0)
+                Assert.Equal(0, result);
+            if (expected < 0)
+                Assert.InRange(result, int.MinValue, -1);
+            if (expected > 0)
+                Assert.InRange(result, +1, int.MaxValue);
+        }
+
+        [Theory]
+        [InlineData(true, null)]
+        [InlineData(true, "2017-01-01")]
+        public void WithinTestUnconstrained(bool expected, string valueS)
+        {
+            var value = valueS == null ? (DateTime?)null : DateTime.Parse(valueS);
+            Assert.Equal(expected, DateHelper.Within(value, DateFilter.Unconstrained));
+        }
+
+        [Theory]
+        [InlineData(true, null)]
+        [InlineData(false, "2017-01-01")]
+        public void WithinTestNullOnly(bool expected, string valueS)
+        {
+            var value = valueS == null ? (DateTime?)null : DateTime.Parse(valueS);
+            Assert.Equal(expected, DateHelper.Within(value, DateFilter.TheNullOnly));
+        }
+
+        [Theory]
+        [InlineData(true, null)]
+        [InlineData(true, "2016-12-31")]
+        [InlineData(true, "2017-01-01")]
+        [InlineData(false, "2017-01-02")]
+        public void WithinTestBy(bool expected, string valueS)
+        {
+            var value = valueS == null ? (DateTime?)null : DateTime.Parse(valueS);
+            var filter = new DateFilter(null, new DateTime(2017, 1, 1));
+            Assert.True(filter.Nullable);
+            Assert.Equal(expected, DateHelper.Within(value, filter));
+        }
+
+        [Theory]
+        [InlineData(false, null)]
+        [InlineData(true, "2016-12-31")]
+        [InlineData(true, "2017-01-01")]
+        [InlineData(false, "2017-01-02")]
+        public void WithinTestByA(bool expected, string valueS)
+        {
+            var value = valueS == null ? (DateTime?)null : DateTime.Parse(valueS);
+            var filter = new DateFilter(null, new DateTime(2017, 1, 1)) { Nullable = false };
+            Assert.Equal(expected, DateHelper.Within(value, filter));
+        }
+
+        [Theory]
+        [InlineData(false, null)]
+        [InlineData(false, "2016-12-31")]
+        [InlineData(true, "2017-01-01")]
+        [InlineData(true, "2017-01-02")]
+        public void WithinTestSince(bool expected, string valueS)
+        {
+            var value = valueS == null ? (DateTime?)null : DateTime.Parse(valueS);
+            var filter = new DateFilter(new DateTime(2017, 1, 1), null);
+            Assert.False(filter.Nullable);
+            Assert.Equal(expected, DateHelper.Within(value, filter));
+        }
+
+        [Theory]
+        [InlineData(true, null)]
+        [InlineData(false, "2016-12-31")]
+        [InlineData(true, "2017-01-01")]
+        [InlineData(true, "2017-01-02")]
+        public void WithinTestSinceA(bool expected, string valueS)
+        {
+            var value = valueS == null ? (DateTime?)null : DateTime.Parse(valueS);
+            var filter = new DateFilter(new DateTime(2017, 1, 1), null) { Nullable = true };
+            Assert.Equal(expected, DateHelper.Within(value, filter));
+        }
+    }
+}
