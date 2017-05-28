47254c33083eeb57c25d04bd6b3b8187ff70590b
b1f6c1c4 <b1f6c1c4@gmail.com>
2017-05-28 21:12:54 +0800

    move concrete composites

--- AccountingServer.Entities/Util/QueryBase.cs
+++ AccountingServer.Entities/Util/QueryBase.cs
@@ -1,102 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace AccountingServer.Entities.Util
+﻿namespace AccountingServer.Entities.Util
 {
-    public class DetailQueryAtomBase : IDetailQueryAtom
+    public sealed class DetailQueryUnconstrained : IDetailQueryAtom
     {
-        public DetailQueryAtomBase(VoucherDetail filter, int dir = 0)
-        {
-            Filter = filter;
-            Dir = dir;
-        }
+        public static IDetailQueryAtom Instance { get; } = new DetailQueryUnconstrained();
 
-        public VoucherDetail Filter { get; }
-        public int Dir { get; }
+        private DetailQueryUnconstrained() { }
+
+        public VoucherDetail Filter { get; } = new VoucherDetail();
+
+        public int Dir => 1;
 
         public void Accept(IQueryVisitor<IDetailQueryAtom> visitor) => visitor.Visit(this);
         public T Accept<T>(IQueryVisitor<IDetailQueryAtom, T> visitor) => visitor.Visit(this);
     }
 
-    public class QueryAryBase<TAtom> : IQueryAry<TAtom> where TAtom : class
+    public sealed class VoucherQueryUnconstrained : IVoucherQueryAtom
     {
-        protected QueryAryBase(OperatorType op, IList<IQueryCompunded<TAtom>> queries)
-        {
-            Operator = op;
-            if (queries.Count == 0)
-                throw new ArgumentException("参与运算的检索式个数过少", nameof(queries));
+        public static IVoucherQueryAtom Instance { get; } = new VoucherQueryUnconstrained();
 
-            Filter1 = queries[0];
-            switch (op)
-            {
-                case OperatorType.Identity:
-                case OperatorType.Complement:
-                    break;
-                case OperatorType.Substract:
-                    Filter2 = queries[1];
-                    break;
-                case OperatorType.Union:
-                case OperatorType.Intersect:
-                    if (queries.Count == 1)
-                        Operator = OperatorType.Identity;
-                    else if (queries.Count == 2)
-                        Filter2 = queries[1];
-                    else
-                        Filter2 = new QueryAryBase<TAtom>(op, queries.Skip(1).ToList());
-                    break;
-                default:
-                    throw new ArgumentException("运算类型未知", nameof(op));
-            }
-        }
+        private VoucherQueryUnconstrained() { }
 
-        public OperatorType Operator { get; }
-        public IQueryCompunded<TAtom> Filter1 { get; }
-        public IQueryCompunded<TAtom> Filter2 { get; }
+        public bool ForAll => true;
 
-        public void Accept(IQueryVisitor<TAtom> visitor) => visitor.Visit(this);
-        public T Accept<T>(IQueryVisitor<TAtom, T> visitor) => visitor.Visit(this);
-    }
+        public Voucher VoucherFilter { get; } = new Voucher();
 
-    public class DetailQueryAryBase : QueryAryBase<IDetailQueryAtom>
-    {
-        private DetailQueryAryBase(OperatorType op, IList<IQueryCompunded<IDetailQueryAtom>> queries) : base(
-            op,
-            queries) { }
+        public DateFilter Range => DateFilter.Unconstrained;
 
-        public DetailQueryAryBase(IEnumerable<VoucherDetail> filters, bool useAnd, int dir = 0)
-            : this(
-                useAnd ? OperatorType.Intersect : OperatorType.Union,
-                filters.Select(filter => new DetailQueryAtomBase(filter, dir))
-                    .Cast<IQueryCompunded<IDetailQueryAtom>>()
-                    .ToList()) { }
-    }
-
-    public struct VoucherQueryAtomBase : IVoucherQueryAtom
-    {
-        public VoucherQueryAtomBase(Voucher vfilter = null, IEnumerable<VoucherDetail> filters = null,
-            DateFilter? rng = null, bool useAnd = false, int dir = 0,
-            bool forAll = false)
-            : this()
-        {
-            VoucherFilter = vfilter;
-            ForAll = forAll;
-            Range = rng ?? DateFilter.Unconstrained;
-            DetailFilter = new DetailQueryAryBase(filters, useAnd, dir);
-        }
-
-        public bool ForAll { get; set; }
-        public Voucher VoucherFilter { get; }
-        public DateFilter Range { get; }
-        public IQueryCompunded<IDetailQueryAtom> DetailFilter { get; set; }
+        public IQueryCompunded<IDetailQueryAtom> DetailFilter => DetailQueryUnconstrained.Instance;
 
         public void Accept(IQueryVisitor<IVoucherQueryAtom> visitor) => visitor.Visit(this);
         public T Accept<T>(IQueryVisitor<IVoucherQueryAtom, T> visitor) => visitor.Visit(this);
-    }
-
-    public class VoucherQueryAryBase : QueryAryBase<IVoucherQueryAtom>
-    {
-        public VoucherQueryAryBase(OperatorType op, IList<IQueryCompunded<IVoucherQueryAtom>> queries)
-            : base(op, queries) { }
     }
 }
