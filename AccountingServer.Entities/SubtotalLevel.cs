fd67320ca1822fe8741a2a02f5487b5f93aa1d89
b1f6c1c4 <b1f6c1c4@gmail.com>
2017-06-08 22:36:12 +0800

    aggr date

--- AccountingServer.Entities/SubtotalLevel.cs
+++ AccountingServer.Entities/SubtotalLevel.cs
@@ -11,51 +11,51 @@ namespace AccountingServer.Entities
         /// <summary>
         ///     不加分类
         /// </summary>
-        None = 0x0,
+        None = 0b0000_0000_0000,
 
         /// <summary>
         ///     按一级科目分类
         /// </summary>
-        Title = 0x1,
+        Title = 0b0000_0000_0001,
 
         /// <summary>
         ///     按二级科目分类
         /// </summary>
-        SubTitle = 0x2,
+        SubTitle = 0b0000_0000_0010,
 
         /// <summary>
         ///     按内容分类
         /// </summary>
-        Content = 0x4,
+        Content = 0b0000_0000_0100,
 
         /// <summary>
         ///     按备注分类
         /// </summary>
-        Remark = 0x8,
+        Remark = 0b0000_0000_1000,
 
         /// <summary>
         ///     按币种分类
         /// </summary>
-        Currency = 0x10,
+        Currency = 0b0000_0001_0000,
 
         /// <summary>
         ///     按日期分类
         /// </summary>
-        Day = 0x20,
+        Day = 0b0000_0010_0000,
 
         /// <summary>
         ///     按周分类
         /// </summary>
-        Week = 0x60,
+        Week = 0b0000_0110_0000,
 
         /// <summary>
         ///     按月分类
         /// </summary>
-        Month = 0xe0,
+        Month = 0b0000_1110_0000,
 
         /// <summary>
         ///     按年分类
         /// </summary>
-        Year = 0x260
+        Year = 0b0010_0110_0000
     }
 }
