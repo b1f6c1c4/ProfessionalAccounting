a89ed5d7e211b874b34beb562b59fee1570ddd83
b1f6c1c4 <b1f6c1c4@gmail.com>
2017-07-16 00:31:03 -0400

    query credit card

--- AccountingServer.Shell/Plugins/CreditCardConvert/CreditCardConvert.cs
+++ AccountingServer.Shell/Plugins/CreditCardConvert/CreditCardConvert.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using AccountingServer.BLL;
+using AccountingServer.BLL.Util;
 using AccountingServer.Entities;
+using AccountingServer.Entities.Util;
 using AccountingServer.Shell.Serializer;
 using AccountingServer.Shell.Util;
 using static AccountingServer.BLL.Parsing.Facade;
@@ -23,6 +26,110 @@ namespace AccountingServer.Shell.Plugins.CreditCardConvert
         public override IQueryResult Execute(string expr)
         {
             var content = Parsing.Token(ref expr);
+
+            return ParsingF.Optional(ref expr, "q") ? Query(content, ref expr) : Create(content, ref expr);
+        }
+
+        private sealed class Trans
+        {
+            public DateTime? Date;
+            public string RawCurrency;
+            public double RawFund;
+            public Conversion TheConversion;
+        }
+
+        private sealed class Conversion
+        {
+            public DateTime? Date;
+
+            public string OriginCurrency;
+            public double OriginFund;
+
+            public string TargetCurrency;
+            public double TargetFund;
+        }
+
+        private IQueryResult Query(string content, ref string expr)
+        {
+            var rng = Parsing.Range(ref expr) ?? DateFilter.Unconstrained;
+
+            var trans = new List<Trans>();
+            var convs = new List<Conversion>();
+            foreach (var voucher in Accountant.RunVoucherQuery($"T2241 {content.Quotation('\'')} {rng.AsDateRange()}"))
+            {
+                var ds = voucher.Details.Where(d => d.Title == 2241 && d.Content == content).ToList();
+                if (ds.Count > 2)
+                    throw new ApplicationException("过多相关细目");
+                if (ds.Count == 2)
+                {
+                    // ReSharper disable PossibleInvalidOperationException
+                    var v1 = ds[0].Fund.Value;
+                    var v2 = ds[1].Fund.Value;
+                    // ReSharper restore PossibleInvalidOperationException
+                    if (v1 < 0 &&
+                        v2 > 0)
+                        convs.Add(
+                            new Conversion
+                                {
+                                    Date = voucher.Date,
+                                    TargetCurrency = ds[0].Currency,
+                                    TargetFund = -v1,
+                                    OriginCurrency = ds[1].Currency,
+                                    OriginFund = v2
+                                });
+                    else if (v1 > 0 &&
+                        v2 < 0)
+                        convs.Add(
+                            new Conversion
+                                {
+                                    Date = voucher.Date,
+                                    TargetCurrency = ds[1].Currency,
+                                    TargetFund = -v2,
+                                    OriginCurrency = ds[0].Currency,
+                                    OriginFund = v1
+                                });
+                    else
+                        throw new ApplicationException("没有买入");
+                }
+                // ReSharper disable once PossibleInvalidOperationException
+                else if (ds[0].Fund.Value < 0)
+                    trans.Add(
+                        new Trans
+                            {
+                                Date = voucher.Date,
+                                RawCurrency = ds[0].Currency,
+                                RawFund = -ds[0].Fund.Value
+                            });
+            }
+
+            foreach (var tran in trans)
+            {
+                var conv = convs.FirstOrDefault(
+                    c => c.Date >= tran.Date && c.OriginCurrency == tran.RawCurrency &&
+                        (c.OriginFund - tran.RawFund).IsZero());
+                if (conv == null)
+                    continue;
+
+                tran.TheConversion = conv;
+                convs.Remove(conv);
+            }
+
+            var sb = new StringBuilder();
+            foreach (var tran in trans.OrderByDescending(t => t.Date))
+            {
+                sb.Append($"{tran.Date.AsDate()} @{tran.RawCurrency} {tran.RawFund.AsCurrency().CPadLeft(15)}");
+                if (tran.TheConversion != null)
+                    sb.AppendLine(
+                        $" {tran.TheConversion.Date.AsDate()} @{tran.TheConversion.TargetCurrency} {tran.TheConversion.TargetFund.AsCurrency().CPadLeft(15)}");
+                else
+                    sb.AppendLine();
+            }
+
+            return new UnEditableText(sb.ToString());
+        }
+
+        private IQueryResult Create(string content, ref string expr)
+        {
             var currency = Parsing.Token(ref expr, false);
 
             var sb = new StringBuilder();
