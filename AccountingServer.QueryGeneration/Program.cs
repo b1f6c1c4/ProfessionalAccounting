0821904fd969430ea7a70e0191cd78b0d4d41cca
b1f6c1c4 <b1f6c1c4@gmail.com>
2018-07-26 19:38:42 +0800

    grammer debugger

--- AccountingServer.QueryGeneration/Program.cs
+++ AccountingServer.QueryGeneration/Program.cs
@@ -1,7 +1,44 @@
-﻿namespace AccountingServer.QueryGeneration
+﻿using System;
+using System.IO;
+using System.Reflection;
+using AccountingServer.BLL.Parsing;
+using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+
+namespace AccountingServer.QueryGeneration
 {
     internal class Program
     {
-        private static void Main(string[] args) { }
+        private static void Main(string[] args)
+        {
+            var method = "voucherQuery";
+            var inputStream = new StreamReader(Console.OpenStandardInput());
+            while (true)
+            {
+                Console.Write($@"({method})> ");
+                var line = inputStream.ReadLine();
+                if (line.StartsWith("use "))
+                {
+                    method = line.Substring(4).Trim();
+                    continue;
+                }
+
+                if (line.StartsWith("exit") ||
+                    line.StartsWith("quit"))
+                    return;
+
+                var input = new AntlrInputStream(line);
+                var lexer = new QueryLexer(input);
+                var tokens = new CommonTokenStream(lexer);
+                var parser = new QueryParser(tokens);
+                var tree = CallParser(parser, method);
+                Console.WriteLine(tree.ToStringTree(parser));
+            }
+        }
+
+        private static IParseTree CallParser(QueryParser parser, string name)
+            => (IParseTree)typeof(QueryParser)
+                .GetMethod(name, BindingFlags.Public | BindingFlags.Instance)
+                .Invoke(parser, new object[0]);
     }
 }
