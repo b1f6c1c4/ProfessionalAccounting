f15a479ce407f0d65838219b75441dad4e442fd2
b1f6c1c4 <b1f6c1c4@gmail.com>
2018-08-31 14:00:44 -0400

    interest expense

--- AccountingServer.Shell/Plugins/Interest/InterestBase.cs
+++ AccountingServer.Shell/Plugins/Interest/InterestBase.cs
@@ -45,38 +45,44 @@ namespace AccountingServer.Shell.Plugins.Interest
             var endDate = !all ? Parsing.UniqueTime(ref expr) : null;
             Parsing.Eof(expr);
 
-            var loans = Accountant.RunGroupedQuery($"T{MajorTitle().AsTitle()}-\"\" ``rc").Items.Cast<ISubtotalRemark>()
+            var loans = Accountant.RunGroupedQuery($"T{MajorTitle().AsTitle()}-\"\" ``rcC").Items
+                .Cast<ISubtotalRemark>()
                 .ToList();
             var rmkObj =
                 loans.Single(
-                        b =>
-                            b.Remark?.StartsWith(remark, StringComparison.InvariantCultureIgnoreCase) == true &&
-                            !b.Remark.EndsWith("-利息", StringComparison.Ordinal));
-            var content = rmkObj.Items.Cast<ISubtotalContent>().Single().Content;
+                    b =>
+                        b.Remark?.StartsWith(remark, StringComparison.InvariantCultureIgnoreCase) == true &&
+                        !b.Remark.EndsWith("-利息", StringComparison.Ordinal));
+            var cntObj = rmkObj.Items.Cast<ISubtotalContent>().Single();
+            var content = cntObj.Content;
             var rmk = rmkObj.Remark;
+            var currency = cntObj.Items.Cast<ISubtotalCurrency>().Single().Currency;
+            var info = new LoanInfo
+                {
+                    Currency = currency,
+                    Content = content,
+                    Remark = rmk,
+                    Rate = rate
+                };
 
             if (!all && !endDate.HasValue ||
                 endDate.HasValue)
             {
-                var filter = $"T{MajorTitle().AsTitle()} {content.Quotation('\'')} {rmk.Quotation('"')}";
-                var filter0 = $"T{MajorTitle().AsTitle()} {content.Quotation('\'')} {(rmk + "-利息").Quotation('"')}";
                 // ReSharper disable once PossibleInvalidOperationException
-                var lastD = Accountant.RunVoucherQuery(filter0)
+                var lastD = Accountant.RunVoucherQuery(info.QueryInterest(this))
                         .OrderByDescending(v => v.Date, new DateComparer())
                         .FirstOrDefault()
                         ?.Date ??
-                    Accountant.RunVoucherQuery(filter)
+                    Accountant.RunVoucherQuery(info.QueryCapital(this))
                         .OrderBy(v => v.Date, new DateComparer())
                         .First()
                         .Date.Value;
-                var capQuery = $"{filter} [~{lastD.AsDate()}]``v";
-                var intQuery = $"{filter0} [~{lastD.AsDate()}]``v";
+                var capQuery = $"{info.QueryCapital(this)} [~{lastD.AsDate()}]``v";
+                var intQuery = $"{info.QueryInterest(this)} [~{lastD.AsDate()}]``v";
                 var capitalIntegral = Accountant.RunGroupedQuery(capQuery).Fund;
                 var interestIntegral = Accountant.RunGroupedQuery(intQuery).Fund;
                 Regularize(
-                    content,
-                    rmk,
-                    rate,
+                    info,
                     ref capitalIntegral,
                     ref interestIntegral,
                     lastD,
@@ -87,9 +93,7 @@ namespace AccountingServer.Shell.Plugins.Interest
                 var capitalIntegral = 0D;
                 var interestIntegral = 0D;
                 Regularize(
-                    content,
-                    rmk,
-                    rate,
+                    info,
                     ref capitalIntegral,
                     ref interestIntegral,
                     null,
@@ -102,35 +106,22 @@ namespace AccountingServer.Shell.Plugins.Interest
         /// <summary>
         ///     从上次计息日后一日起计算单利利息并整理还款
         /// </summary>
-        /// <param name="content">借款人</param>
-        /// <param name="rmk">借款代码</param>
-        /// <param name="rate">利率</param>
+        /// <param name="info">借款信息</param>
         /// <param name="capitalIntegral">剩余本金</param>
         /// <param name="interestIntegral">剩余利息</param>
         /// <param name="lastSettlement">上次计息日</param>
         /// <param name="finalDay">截止日期</param>
-        private void Regularize(string content, string rmk, double rate, ref double capitalIntegral,
-            ref double interestIntegral, DateTime? lastSettlement, DateTime finalDay)
+        private void Regularize(LoanInfo info, ref double capitalIntegral, ref double interestIntegral,
+            DateTime? lastSettlement, DateTime finalDay)
         {
-            var capitalPattern = new VoucherDetail
-            {
-                Title = MajorTitle(),
-                Content = content,
-                Remark = rmk
-            };
-            var interestPattern = new VoucherDetail
-            {
-                Title = MajorTitle(),
-                Content = content,
-                Remark = rmk + "-利息"
-            };
+            var capitalPattern = info.AsCapital(this);
+            var interestPattern = info.AsInterest(this);
             var rng = lastSettlement.HasValue
                 ? new DateFilter(lastSettlement.Value.AddDays(1), finalDay)
                 : new DateFilter(null, finalDay);
             foreach (var grp in
                 Accountant
-                    .RunVoucherQuery(
-                        $"(T{MajorTitle().AsTitle()} {content.Quotation('\'')})*({rmk.Quotation('"')}+{(rmk + "-利息").Quotation('"')}) {rng.AsDateRange()}")
+                    .RunVoucherQuery($"{info.QueryMajor(this)} {rng.AsDateRange()}")
                     .GroupBy(v => v.Date)
                     .OrderBy(grp => grp.Key, new DateComparer()))
             {
@@ -141,25 +132,23 @@ namespace AccountingServer.Shell.Plugins.Interest
 
                 // Settle Interest
                 interestIntegral += SettleInterest(
-                    content,
-                    rmk,
-                    rate,
+                    info,
                     capitalIntegral,
                     key.Subtract(lastSettlement.Value).Days,
                     grp.SingleOrDefault(
                         v =>
-                            v.Details.Any(d => d.IsMatch(interestPattern, 1)))
+                            v.Details.Any(d => d.IsMatch(interestPattern, Dir())))
                     ?? new Voucher
-                    {
-                        Date = key,
-                        Details = new List<VoucherDetail>()
-                    });
+                        {
+                            Date = key,
+                            Details = new List<VoucherDetail>()
+                        });
                 lastSettlement = key;
 
                 // Settle Loan
                 // ReSharper disable once PossibleInvalidOperationException
                 capitalIntegral +=
-                    grp.SelectMany(v => v.Details.Where(d => d.IsMatch(capitalPattern, 1)))
+                    grp.SelectMany(v => v.Details.Where(d => d.IsMatch(capitalPattern, Dir())))
                         .Select(d => d.Fund.Value)
                         .Sum();
 
@@ -168,22 +157,24 @@ namespace AccountingServer.Shell.Plugins.Interest
                     var voucher in
                     grp.Where(
                             v =>
-                                v.Details.Any(d => d.IsMatch(capitalPattern, -1) || d.IsMatch(interestPattern, -1)))
+                                v.Details.Any(
+                                    d => d.IsMatch(capitalPattern, -Dir()) || d.IsMatch(interestPattern, -Dir())))
                         .OrderBy(v => v.ID))
                 {
                     // ReSharper disable once PossibleInvalidOperationException
                     var value =
-                        -voucher.Details.Where(d => d.IsMatch(capitalPattern, -1) || d.IsMatch(interestPattern, -1))
+                        -voucher.Details.Where(
+                                d => d.IsMatch(capitalPattern, -Dir()) || d.IsMatch(interestPattern, -Dir()))
                             .Select(d => d.Fund.Value)
                             .Sum();
-                    if ((-value + interestIntegral).IsNonNegative())
+                    if ((Dir() * (-value + interestIntegral)).IsNonNegative())
                     {
-                        RegularizeVoucherDetail(content, rmk, voucher, 0, value);
+                        RegularizeVoucherDetail(info, voucher, 0, value);
                         interestIntegral -= value;
                     }
                     else
                     {
-                        RegularizeVoucherDetail(content, rmk, voucher, value - interestIntegral, interestIntegral);
+                        RegularizeVoucherDetail(info, voucher, value - interestIntegral, interestIntegral);
                         capitalIntegral -= value - interestIntegral;
                         interestIntegral = 0;
                     }
@@ -195,64 +186,49 @@ namespace AccountingServer.Shell.Plugins.Interest
 
             if (lastSettlement != finalDay)
                 interestIntegral += SettleInterest(
-                    content,
-                    rmk,
-                    rate,
+                    info,
                     capitalIntegral,
                     finalDay.Subtract(lastSettlement.Value).Days,
                     new Voucher
-                    {
-                        Date = finalDay,
-                        Details = new List<VoucherDetail>()
-                    });
+                        {
+                            Date = finalDay,
+                            Details = new List<VoucherDetail>()
+                        });
         }
 
         /// <summary>
         ///     计算利息
         /// </summary>
-        /// <param name="content">借款人</param>
-        /// <param name="rmk">借款代码</param>
-        /// <param name="rate">利率</param>
+        /// <param name="info">借款信息</param>
         /// <param name="capitalIntegral">剩余本金</param>
         /// <param name="delta">间隔日数</param>
         /// <param name="voucher">记账凭证</param>
         /// <returns>利息</returns>
-        private double SettleInterest(string content, string rmk, double rate,
-            double capitalIntegral, int delta, Voucher voucher)
+        private double SettleInterest(LoanInfo info, double capitalIntegral, int delta, Voucher voucher)
         {
-            var interestPattern = new VoucherDetail
-            {
-                Title = MajorTitle(),
-                Content = content,
-                Remark = rmk + "-利息"
-            };
-            var revenuePattern = new VoucherDetail
-            {
-                Title = 6603,
-                SubTitle = MinorSubTitle(),
-                Content = "贷款利息"
-            };
-            var interest = delta * rate * capitalIntegral;
+            var interest = delta * info.Rate * capitalIntegral;
             var create = new List<VoucherDetail>
                 {
-                    new VoucherDetail
-                        {
-                            Title = MajorTitle(),
-                            Content = content,
-                            Remark = rmk + "-利息",
-                            Fund = interest
-                        },
-                    new VoucherDetail
-                        {
-                            Title = 6603,
-                            SubTitle = MinorSubTitle(),
-                            Content = "贷款利息",
-                            Fund = -interest
-                        }
+                    info.AsInterest(this, interest),
+                    info.AsMinor(this, -interest)
                 };
 
             // ReSharper disable once PossibleInvalidOperationException
-            var detail = voucher.Details.SingleOrDefault(d => d.IsMatch(interestPattern));
+            var detail = voucher.Details.SingleOrDefault(d => d.IsMatch(info.AsInterest(this)));
+
+            if (interest.IsZero())
+            {
+                if (detail != null)
+                {
+                    if (!voucher.Details.All(d => d.IsMatch(info.AsInterest(this)) || d.IsMatch(info.AsMinor(this))))
+                        throw new ArgumentException("该记账凭证包含计息以外的细目", nameof(voucher));
+
+                    Accountant.DeleteVoucher(voucher.ID);
+                }
+
+                return 0;
+            }
+
             if (detail == null)
             {
                 voucher.Details = create;
@@ -261,7 +237,7 @@ namespace AccountingServer.Shell.Plugins.Interest
             // ReSharper disable once PossibleInvalidOperationException
             else if (!(detail.Fund.Value - interest).IsZero())
             {
-                if (!voucher.Details.All(d => d.IsMatch(interestPattern) || d.IsMatch(revenuePattern)))
+                if (!voucher.Details.All(d => d.IsMatch(info.AsInterest(this)) || d.IsMatch(info.AsMinor(this))))
                     throw new ArgumentException("该记账凭证包含计息以外的细目", nameof(voucher));
 
                 voucher.Details = create;
@@ -274,31 +250,18 @@ namespace AccountingServer.Shell.Plugins.Interest
         /// <summary>
         ///     正确登记还款
         /// </summary>
-        /// <param name="content">借款人</param>
-        /// <param name="rmk">借款代码</param>
+        /// <param name="info">借款信息</param>
         /// <param name="voucher">记账凭证</param>
         /// <param name="capVol">本金还款额</param>
         /// <param name="intVol">利息还款额</param>
-        private void RegularizeVoucherDetail(string content, string rmk, Voucher voucher, double capVol, double intVol)
+        private void RegularizeVoucherDetail(LoanInfo info, Voucher voucher, double capVol, double intVol)
         {
-            var capitalPattern = new VoucherDetail
-            {
-                Title = MajorTitle(),
-                Content = content,
-                Remark = rmk
-            };
-            var interestPattern = new VoucherDetail
-            {
-                Title = MajorTitle(),
-                Content = content,
-                Remark = rmk + "-利息"
-            };
             var flag = false;
             var capFlag = false;
             var intFlag = false;
             for (var i = 0; i < voucher.Details.Count; i++)
             {
-                if (voucher.Details[i].IsMatch(capitalPattern, -1))
+                if (voucher.Details[i].IsMatch(info.AsCapital(this), -Dir()))
                 {
                     if (capFlag || capVol.IsZero())
                     {
@@ -318,7 +281,7 @@ namespace AccountingServer.Shell.Plugins.Interest
                     capFlag = true;
                 }
 
-                if (voucher.Details[i].IsMatch(interestPattern, -1))
+                if (voucher.Details[i].IsMatch(info.AsInterest(this), -Dir()))
                 {
                     if (intFlag || intVol.IsZero())
                     {
@@ -342,33 +305,81 @@ namespace AccountingServer.Shell.Plugins.Interest
             if (!capFlag &&
                 !capVol.IsZero())
             {
-                voucher.Details.Add(
-                    new VoucherDetail
-                    {
-                        Title = MajorTitle(),
-                        Content = content,
-                        Remark = rmk,
-                        Fund = -capVol
-                    });
+                voucher.Details.Add(info.AsCapital(this, -capVol));
                 flag = true;
             }
 
             if (!intFlag &&
                 !intVol.IsZero())
             {
-                voucher.Details.Add(
-                    new VoucherDetail
-                    {
-                        Title = MajorTitle(),
-                        Content = content,
-                        Remark = rmk + "-利息",
-                        Fund = -intVol
-                    });
+                voucher.Details.Add(info.AsInterest(this, -intVol));
                 flag = true;
             }
 
             if (flag)
                 Accountant.Upsert(voucher);
+        }
+
+        private sealed class LoanInfo
+        {
+            /// <summary>
+            ///     币种
+            /// </summary>
+            public string Currency { private get; set; }
+
+            /// <summary>
+            ///     借款人
+            /// </summary>
+            public string Content { private get; set; }
+
+            /// <summary>
+            ///     借款代码
+            /// </summary>
+            public string Remark { private get; set; }
+
+            /// <summary>
+            ///     日利率
+            /// </summary>
+            public double Rate { get; set; }
+
+            public string QueryMajor(InterestBase my) =>
+                $"(@{Currency} T{my.MajorTitle().AsTitle()} {Content.Quotation('\'')})*({Remark.Quotation('"')}+{(Remark + "-利息").Quotation('"')})";
+
+            public string QueryCapital(InterestBase my) =>
+                $"@{Currency} T{my.MajorTitle().AsTitle()} {Content.Quotation('\'')} {Remark.Quotation('"')}";
+
+            public string QueryInterest(InterestBase my) =>
+                $"@{Currency} T{my.MajorTitle().AsTitle()} {Content.Quotation('\'')} {(Remark + "-利息").Quotation('"')}";
+
+            public VoucherDetail AsCapital(InterestBase my, double? fund = null)
+                => new VoucherDetail
+                    {
+                        Currency = Currency,
+                        Title = my.MajorTitle(),
+                        Content = Content,
+                        Remark = Remark,
+                        Fund = fund
+                    };
+
+            public VoucherDetail AsInterest(InterestBase my, double? fund = null)
+                => new VoucherDetail
+                    {
+                        Currency = Currency,
+                        Title = my.MajorTitle(),
+                        Content = Content,
+                        Remark = Remark + "-利息",
+                        Fund = fund
+                    };
+
+            public VoucherDetail AsMinor(InterestBase my, double? fund = null)
+                => new VoucherDetail
+                    {
+                        Currency = Currency,
+                        Title = 6603,
+                        SubTitle = my.MinorSubTitle(),
+                        Content = "贷款利息",
+                        Fund = fund
+                    };
         }
     }
 }
