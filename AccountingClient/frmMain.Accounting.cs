10a86eb257a2ddc4767d471b0763448f85ef2a89
b1f6c1c4 <b1f6c1c4@gmail.com>
2019-01-04 19:04:39 -0500

    async

--- AccountingClient/frmMain.Accounting.cs
+++ AccountingClient/frmMain.Accounting.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Drawing;
+using System.Threading.Tasks;
 using AccountingClient.Shell;
 
 namespace AccountingClient
@@ -15,43 +16,39 @@ namespace AccountingClient
         /// <summary>
         ///     初始化
         /// </summary>
-        private void PrepareAccounting() => m_Shell = new Facade();
+        private async Task PrepareAccounting()
+        {
+            m_Shell = new Facade();
+            await m_Shell.Init();
+        }
 
         /// <summary>
         ///     批量更新或添加
         /// </summary>
-        private void PerformUpserts()
+        private async Task PerformUpserts()
         {
-            var pos = scintilla.TextLength;
-            while (PerformUpsert(out pos, pos)) { }
+            int? pos = scintilla.TextLength;
+            while ((pos = await PerformUpsert(pos)).HasValue) { }
         }
 
         /// <summary>
         ///     更新或添加
         /// </summary>
-        /// <param name="position">搜索位置</param>
-        /// <returns>是否成功</returns>
-        private bool PerformUpsert(int? position = null) => PerformUpsert(out _, position);
-
-        /// <summary>
-        ///     更新或添加
-        /// </summary>
-        /// <param name="newPosition">搜索位置</param>
         /// <param name="position">开始位置</param>
-        /// <returns>是否成功</returns>
-        private bool PerformUpsert(out int newPosition, int? position = null)
+        /// <returns>若成功则为搜索位置，否则为<c>null</c></returns>
+        private async Task<int?> PerformUpsert(int? position = null)
         {
-            newPosition = position ?? scintilla.SelectionEnd;
+            var newPosition = position ?? scintilla.SelectionEnd;
 
             if (!GetEditableText(newPosition, out var begin, out var end, out var typeName))
-                return false;
+                return null;
 
             try
             {
                 var s = scintilla.Text.Substring(begin, end - begin + 1).Trim().Trim('@');
-                var result = ExecuteUpsert(typeName, s);
+                var result = await ExecuteUpsert(typeName, s);
                 if (result == null)
-                    return false;
+                    return null;
 
                 if (scintilla.Text[end] == '\n' &&
                     result[result.Length - 1] != '\n')
@@ -75,7 +72,7 @@ namespace AccountingClient
             }
 
             scintilla.ScrollCaret();
-            return true;
+            return newPosition;
         }
 
         /// <summary>
@@ -84,19 +81,19 @@ namespace AccountingClient
         /// <param name="typeName">类型名称</param>
         /// <param name="s">表示</param>
         /// <returns>执行结果</returns>
-        private string ExecuteUpsert(string typeName, string s)
+        private async Task<string> ExecuteUpsert(string typeName, string s)
         {
             string result;
             switch (typeName)
             {
                 case "Voucher":
-                    result = m_Shell.ExecuteVoucherUpsert(s);
+                    result = await m_Shell.ExecuteVoucherUpsert(s);
                     break;
                 case "Asset":
-                    result = m_Shell.ExecuteAssetUpsert(s);
+                    result = await m_Shell.ExecuteAssetUpsert(s);
                     break;
                 case "Amortization":
-                    result = m_Shell.ExecuteAmortUpsert(s);
+                    result = await m_Shell.ExecuteAmortUpsert(s);
                     break;
                 default:
                     return null;
@@ -108,36 +105,28 @@ namespace AccountingClient
         /// <summary>
         ///     批量删除
         /// </summary>
-        private void PerformRemovals()
+        private async Task PerformRemovals()
         {
-            var pos = scintilla.TextLength;
-            while (PerformRemoval(out pos, pos)) { }
+            int? pos = scintilla.TextLength;
+            while ((pos = await PerformRemoval(pos)).HasValue) { }
         }
 
         /// <summary>
         ///     删除
         /// </summary>
-        /// <param name="position">搜索位置</param>
-        /// <returns>是否成功</returns>
-        private bool PerformRemoval(int? position = null) => PerformRemoval(out _, position);
-
-        /// <summary>
-        ///     删除
-        /// </summary>
-        /// <param name="newPosition">搜索位置</param>
-        /// <param name="position">搜索位置</param>
-        /// <returns>是否成功</returns>
-        private bool PerformRemoval(out int newPosition, int? position = null)
+        /// <param name="position">开始位置</param>
+        /// <returns>若成功则为搜索位置，否则为<c>null</c></returns>
+        private async Task<int?> PerformRemoval(int? position = null)
         {
-            newPosition = position ?? scintilla.SelectionEnd;
+            var newPosition = position ?? scintilla.SelectionEnd;
 
             if (!GetEditableText(newPosition, out var begin, out var end, out var typeName))
-                return false;
+                return null;
 
             try
             {
                 var s = scintilla.Text.Substring(begin, end - begin + 1).Trim().Trim('@');
-                var result = ExecuteRemoval(typeName, s);
+                var result = await ExecuteRemoval(typeName, s);
 
                 if (!result)
                     throw new ApplicationException("提交的内容类型未知");
@@ -163,7 +152,7 @@ namespace AccountingClient
             }
 
             scintilla.ScrollCaret();
-            return true;
+            return newPosition;
         }
 
         /// <summary>
@@ -172,19 +161,19 @@ namespace AccountingClient
         /// <param name="typeName">类型名称</param>
         /// <param name="s">表示</param>
         /// <returns>执行结果</returns>
-        private bool ExecuteRemoval(string typeName, string s)
+        private async Task<bool> ExecuteRemoval(string typeName, string s)
         {
             bool result;
             switch (typeName)
             {
                 case "Voucher":
-                    result = m_Shell.ExecuteVoucherRemoval(s);
+                    result = await m_Shell.ExecuteVoucherRemoval(s);
                     break;
                 case "Asset":
-                    result = m_Shell.ExecuteAssetRemoval(s);
+                    result = await m_Shell.ExecuteAssetRemoval(s);
                     break;
                 case "Amortization":
-                    result = m_Shell.ExecuteAmortRemoval(s);
+                    result = await m_Shell.ExecuteAmortRemoval(s);
                     break;
                 default:
                     return false;
@@ -197,11 +186,11 @@ namespace AccountingClient
         ///     执行表达式
         /// </summary>
         /// <returns></returns>
-        private bool ExecuteCommand(bool append)
+        private async Task<bool> ExecuteCommand(bool append)
         {
             try
             {
-                var res = m_Shell.Execute(textBoxCommand.Text);
+                var res = await m_Shell.Execute(textBoxCommand.Text);
                 if (res == null)
                     return true;
 
