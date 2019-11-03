af734778604dadd97b114177b06b6e5d3ab36e3f
b1f6c1c4 <b1f6c1c4@gmail.com>
2019-11-03 01:54:37 -0500

    enhanced stmt auto

--- AccountingServer.BLL/Util/QueryHelper.cs
+++ AccountingServer.BLL/Util/QueryHelper.cs
@@ -2,16 +2,16 @@
 
 namespace AccountingServer.BLL.Util
 {
-    public sealed class IntersectQueries<TAtom> : IQueryAry<TAtom> where TAtom : class
+    public abstract class BinaryQueries<TAtom> : IQueryAry<TAtom> where TAtom : class
     {
-        public IntersectQueries(IQueryCompunded<TAtom> filter1,
+        protected BinaryQueries(IQueryCompunded<TAtom> filter1,
             IQueryCompunded<TAtom> filter2)
         {
             Filter1 = filter1;
             Filter2 = filter2;
         }
 
-        public OperatorType Operator => OperatorType.Intersect;
+        public abstract OperatorType Operator { get; }
 
         public IQueryCompunded<TAtom> Filter1 { get; }
 
@@ -20,5 +20,21 @@ namespace AccountingServer.BLL.Util
         public bool IsDangerous() => (Filter1?.IsDangerous() ?? false) || (Filter2?.IsDangerous() ?? false);
 
         public T Accept<T>(IQueryVisitor<TAtom, T> visitor) => visitor.Visit(this);
+    }
+
+    public sealed class IntersectQueries<TAtom> : BinaryQueries<TAtom> where TAtom : class
+    {
+        public IntersectQueries(IQueryCompunded<TAtom> filter1,
+            IQueryCompunded<TAtom> filter2) : base(filter1, filter2) { }
+
+        public override OperatorType Operator => OperatorType.Intersect;
+    }
+
+    public sealed class UnionQueries<TAtom> : BinaryQueries<TAtom> where TAtom : class
+    {
+        public UnionQueries(IQueryCompunded<TAtom> filter1,
+            IQueryCompunded<TAtom> filter2) : base(filter1, filter2) { }
+
+        public override OperatorType Operator => OperatorType.Union;
     }
 }
