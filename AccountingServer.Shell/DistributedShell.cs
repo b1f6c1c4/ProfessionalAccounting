60a0385ddfe1440dbd76f42821b9005e215b52a8
b1f6c1c4 <b1f6c1c4@gmail.com>
2020-06-06 21:28:45 -0400

    add copyright and license notice

--- AccountingServer.Shell/DistributedShell.cs
+++ AccountingServer.Shell/DistributedShell.cs
@@ -1,3 +1,21 @@
+/* Copyright (C) 2020 b1f6c1c4
+ *
+ * This file is part of ProfessionalAccounting.
+ *
+ * ProfessionalAccounting is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, version 3.
+ *
+ * ProfessionalAccounting is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with ProfessionalAccounting.  If not, see
+ * <https://www.gnu.org/licenses/>.
+ */
+
 using System;
 using System.Security;
 using AccountingServer.BLL;
