7ab7ec3e0b002e3946746b35880d95b6bacc2151
Laurel Merton <33132228+KevinZonda@users.noreply.github.com>
2021-04-09 17:06:09 +0100

    Http.Code: add 418 I'm a teapot!
    Refer https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/418

--- AccountingServer/Http/ResponseCodes.cs
+++ AccountingServer/Http/ResponseCodes.cs
@@ -59,6 +59,7 @@ namespace AccountingServer.Http
                     { 415, "Unsupported Media Type" },
                     { 416, "Requested Range Not Satisfiable" },
                     { 417, "Expectation Failed" },
+                    { 418, "I'm a teapot" },
                     { 500, "Internal Server Error" },
                     { 501, "Not Implemented" },
                     { 502, "Bad Gateway" },
