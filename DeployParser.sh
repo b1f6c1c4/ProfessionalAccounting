d1eef36bd9467ffbfc5b62c7a41a8ce8503ac087
b1f6c1c4 <b1f6c1c4@gmail.com>
2021-04-12 13:42:35 -0400

    fix generation path

--- DeployParser.sh
+++ DeployParser.sh
@@ -2,8 +2,10 @@
 
 set -euo pipefail
 
-cp AccountingServer.QueryGeneration/obj/Gen/Query* AccountingServer.BLL/Parsing/
-cp AccountingServer.QueryGeneration/obj/Gen/Subtotal* AccountingServer.BLL/Parsing/
+cp AccountingServer.QueryGeneration/obj/Gen/net5.0/Query{.tokens,Lexer.{cs,tokens},Parser.cs} \
+    AccountingServer.BLL/Parsing/
+cp AccountingServer.QueryGeneration/obj/Gen/net5.0/Subtotal{.tokens,Lexer.{cs,tokens},Parser.cs} \
+    AccountingServer.BLL/Parsing/
 
 sed -i 's/^public partial class Query/internal partial class Query/' \
     AccountingServer.BLL/Parsing/QueryLexer.cs \
