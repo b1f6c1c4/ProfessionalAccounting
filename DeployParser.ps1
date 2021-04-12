170f127c78ac254139154577a5ca42e2385a37b9
b1f6c1c4 <b1f6c1c4@gmail.com>
2021-04-12 13:42:35 -0400

    fix generation path

--- DeployParser.ps1
+++ DeployParser.ps1
@@ -1,7 +1,15 @@
 $ErrorActionPreference = "Stop"
 
-cp AccountingServer.QueryGeneration/obj/Gen/Query* AccountingServer.BLL/Parsing/
-cp AccountingServer.QueryGeneration/obj/Gen/Subtotal* AccountingServer.BLL/Parsing/
+cp AccountingServer.QueryGeneration/obj/Gen/net5.0/Query.tokens `
+    AccountingServer.QueryGeneration/obj/Gen/net5.0/QueryLexer.cs `
+    AccountingServer.QueryGeneration/obj/Gen/net5.0/QueryLexer.tokens `
+    AccountingServer.QueryGeneration/obj/Gen/net5.0/QueryParser.cs `
+    AccountingServer.BLL/Parsing/
+cp AccountingServer.SubtotalGeneration/obj/Gen/net5.0/Subtotal.tokens `
+    AccountingServer.SubtotalGeneration/obj/Gen/net5.0/SubtotalLexer.cs `
+    AccountingServer.SubtotalGeneration/obj/Gen/net5.0/SubtotalLexer.tokens `
+    AccountingServer.SubtotalGeneration/obj/Gen/net5.0/SubtotalParser.cs `
+    AccountingServer.BLL/Parsing/
 
 sed '-i.bak' 's/^public partial class Query/internal partial class Query/' `
     AccountingServer.BLL/Parsing/QueryLexer.cs `
