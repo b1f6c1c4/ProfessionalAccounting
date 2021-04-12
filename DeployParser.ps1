c4738e257875e89259e6995c60ca6d4d21f11810
b1f6c1c4 <b1f6c1c4@gmail.com>
2021-04-12 19:39:57 -0400

    fix ci problem

--- DeployParser.ps1
+++ DeployParser.ps1
@@ -1,15 +1,13 @@
 $ErrorActionPreference = "Stop"
 
-cp AccountingServer.QueryGeneration/obj/Gen/net5.0/Query.tokens `
-    AccountingServer.QueryGeneration/obj/Gen/net5.0/QueryLexer.cs `
-    AccountingServer.QueryGeneration/obj/Gen/net5.0/QueryLexer.tokens `
-    AccountingServer.QueryGeneration/obj/Gen/net5.0/QueryParser.cs `
-    AccountingServer.BLL/Parsing/
-cp AccountingServer.SubtotalGeneration/obj/Gen/net5.0/Subtotal.tokens `
-    AccountingServer.SubtotalGeneration/obj/Gen/net5.0/SubtotalLexer.cs `
-    AccountingServer.SubtotalGeneration/obj/Gen/net5.0/SubtotalLexer.tokens `
-    AccountingServer.SubtotalGeneration/obj/Gen/net5.0/SubtotalParser.cs `
-    AccountingServer.BLL/Parsing/
+cp AccountingServer.QueryGeneration/obj/Gen/net5.0/Query.tokens AccountingServer.BLL/Parsing/
+cp AccountingServer.QueryGeneration/obj/Gen/net5.0/QueryLexer.cs AccountingServer.BLL/Parsing/
+cp AccountingServer.QueryGeneration/obj/Gen/net5.0/QueryLexer.tokens AccountingServer.BLL/Parsing/
+cp AccountingServer.QueryGeneration/obj/Gen/net5.0/QueryParser.cs AccountingServer.BLL/Parsing/
+cp AccountingServer.QueryGeneration/obj/Gen/net5.0/Subtotal.tokens AccountingServer.BLL/Parsing/
+cp AccountingServer.QueryGeneration/obj/Gen/net5.0/SubtotalLexer.cs AccountingServer.BLL/Parsing/
+cp AccountingServer.QueryGeneration/obj/Gen/net5.0/SubtotalLexer.tokens AccountingServer.BLL/Parsing/
+cp AccountingServer.QueryGeneration/obj/Gen/net5.0/SubtotalParser.cs AccountingServer.BLL/Parsing/
 
 sed '-i.bak' 's/^public partial class Query/internal partial class Query/' `
     AccountingServer.BLL/Parsing/QueryLexer.cs `
