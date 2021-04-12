32d8980debd7698e6c8d973298f63f704a105ffb
b1f6c1c4 <b1f6c1c4@gmail.com>
2021-04-12 13:54:26 -0400

    fix namespace

--- DeployParser.sh
+++ DeployParser.sh
@@ -7,9 +7,11 @@ cp AccountingServer.QueryGeneration/obj/Gen/net5.0/Query{.tokens,Lexer.{cs,token
 cp AccountingServer.QueryGeneration/obj/Gen/net5.0/Subtotal{.tokens,Lexer.{cs,tokens},Parser.cs} \
     AccountingServer.BLL/Parsing/
 
-sed -i 's/^public partial class Query/internal partial class Query/' \
+sed -i 's/^public partial class Query/internal partial class Query/
+        s/AccountingServer\.QueryGeneration/AccountingServer.BLL.Parsing/' \
     AccountingServer.BLL/Parsing/QueryLexer.cs \
     AccountingServer.BLL/Parsing/QueryParser.cs
-sed -i 's/^public partial class Subtotal/internal partial class Subtotal/' \
+sed -i 's/^public partial class Subtotal/internal partial class Subtotal/
+        s/AccountingServer\.QueryGeneration/AccountingServer.BLL.Parsing/' \
     AccountingServer.BLL/Parsing/SubtotalLexer.cs \
     AccountingServer.BLL/Parsing/SubtotalParser.cs
