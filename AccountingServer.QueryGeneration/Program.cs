d5503d0f536131d51a64a81ee96be90140ea0489
b1f6c1c4 <b1f6c1c4@gmail.com>
2021-04-12 23:40:55 -0400

    use C# 9.0 top level stmt

--- AccountingServer.QueryGeneration/Program.cs
+++ AccountingServer.QueryGeneration/Program.cs
@@ -19,80 +19,71 @@
 using System;
 using System.IO;
 using System.Reflection;
-using AccountingServer.BLL.Parsing;
 using Antlr4.Runtime;
 using Antlr4.Runtime.Tree;
+using AccountingServer.QueryGeneration;
 
-namespace AccountingServer.QueryGeneration
-{
-    internal class Program
+Type GetLexer(string kind)
+=> kind switch
     {
-        private static void Main(string[] args)
+        "Query" => typeof(QueryLexer),
+        "Subtotal" => typeof(SubtotalLexer),
+        _ => throw new ArgumentOutOfRangeException(nameof(kind)),
+    };
+
+Type GetParser(string kind)
+=> kind switch
+    {
+        "Query" => typeof(QueryParser),
+        "Subtotal" => typeof(SubtotalParser),
+        _ => throw new ArgumentOutOfRangeException(nameof(kind)),
+    };
+
+IParseTree CallParser(Parser parser, string name)
+=> (IParseTree)parser
+    .GetType()
+    .GetMethod(name, BindingFlags.Public | BindingFlags.Instance)
+    .Invoke(parser, new object[0]);
+
+var kind = "Query";
+var method = "lexer";
+var inputStream = new StreamReader(Console.OpenStandardInput());
+while (true)
+{
+    Console.Write($"({kind}.{method})> ");
+    var line = inputStream.ReadLine();
+    if (line.StartsWith("use "))
+    {
+        var desc = line[4..].Trim().Split('.');
+        kind = desc[0];
+        method = desc[1];
+        continue;
+    }
+
+    if (line.StartsWith("exit") ||
+        line.StartsWith("quit"))
+        break;
+
+    var input = new AntlrInputStream(line);
+    var lexer = (Lexer)Activator.CreateInstance(GetLexer(kind), input);
+    var tokens = new CommonTokenStream(lexer);
+    if (method == "lexer")
+    {
+        var i = 0;
+        while (true)
         {
-            var kind = "Query";
-            var method = "lexer";
-            var inputStream = new StreamReader(Console.OpenStandardInput());
-            while (true)
-            {
-                Console.Write($"({kind}.{method})> ");
-                var line = inputStream.ReadLine();
-                if (line.StartsWith("use "))
-                {
-                    var desc = line[4..].Trim().Split('.');
-                    kind = desc[0];
-                    method = desc[1];
-                    continue;
-                }
-
-                if (line.StartsWith("exit") ||
-                    line.StartsWith("quit"))
-                    return;
-
-                var input = new AntlrInputStream(line);
-                var lexer = (Lexer)Activator.CreateInstance(GetLexer(kind), input);
-                var tokens = new CommonTokenStream(lexer);
-                if (method == "lexer")
-                {
-                    var i = 0;
-                    while (true)
-                    {
-                        var t = tokens.Lt(++i);
-                        if (t.Type == -1)
-                            break;
-                        var vocabulary = lexer.Vocabulary;
-                        Console.WriteLine(
-                            $"#{t.Type}({vocabulary.GetDisplayName(t.Type)})({vocabulary.GetLiteralName(t.Type)})({vocabulary.GetSymbolicName(t.Type)}) {t.Text}");
-                    }
-
-                    continue;
-                }
-
-                var parser = (Parser)Activator.CreateInstance(GetParser(kind), tokens);
-                var tree = CallParser(parser, method);
-                Console.WriteLine(tree.ToStringTree(parser));
-            }
+            var t = tokens.Lt(++i);
+            if (t.Type == -1)
+                break;
+            var vocabulary = lexer.Vocabulary;
+            Console.WriteLine(
+                $"#{t.Type}({vocabulary.GetDisplayName(t.Type)})({vocabulary.GetLiteralName(t.Type)})({vocabulary.GetSymbolicName(t.Type)}) {t.Text}");
         }
 
-        private static Type GetLexer(string kind)
-            => kind switch
-                {
-                    "Query" => typeof(QueryLexer),
-                    "Subtotal" => typeof(SubtotalLexer),
-                    _ => throw new ArgumentOutOfRangeException(nameof(kind)),
-                };
-
-        private static Type GetParser(string kind)
-            => kind switch
-                {
-                    "Query" => typeof(QueryParser),
-                    "Subtotal" => typeof(SubtotalParser),
-                    _ => throw new ArgumentOutOfRangeException(nameof(kind)),
-                };
-
-        private static IParseTree CallParser(Parser parser, string name)
-            => (IParseTree)parser
-                .GetType()
-                .GetMethod(name, BindingFlags.Public | BindingFlags.Instance)
-                .Invoke(parser, new object[0]);
+        continue;
     }
+
+    var parser = (Parser)Activator.CreateInstance(GetParser(kind), tokens);
+    var tree = CallParser(parser, method);
+    Console.WriteLine(tree.ToStringTree(parser));
 }
