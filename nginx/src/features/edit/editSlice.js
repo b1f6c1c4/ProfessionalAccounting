5c56495bbf867060659418993f6bc4e2df531520
IoriOikawa <fengkaiw@usc.edu>
2021-11-29 21:18:04 -0500

    editSlice added.

---
+++ nginx/src/features/edit/editSlice.js
@@ -0,0 +1,39 @@
+import { createSlice } from '@reduxjs/toolkit';
+import dayjs from 'dayjs';
+
+const initialState = {
+    loading: false,
+    liveViewText: '',
+    editor: {
+        date: dayjs().format('YYYYMMDD'),
+        person: 'anonymous',
+        activity: '',
+        details: [],
+        adjustments: [],
+        payments: [],
+        checksum: 0,
+        commited: false,
+    },
+};
+
+export const editSlice = createSlice({
+    name: 'edit',
+    initialState,
+    reducers: {
+        dateInc: (state) => {
+            const d = dayjs(state.editor.date, 'YYYYMMDD');
+            state.editor.date = d.add(1, 'day').format('YYYYMMDD');
+        },
+        dateDec: (state) => {
+            const d = dayjs(state.editor.date, 'YYYYMMDD');
+            state.editor.date = d.subtract(1, 'day').format('YYYYMMDD');
+        },
+    },
+});
+
+export const {
+    dateInc,
+    dateDec,
+} = editSlice.actions;
+
+export default editSlice.reducer;
