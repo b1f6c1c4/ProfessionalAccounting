6a651ef7829ea4f402e887115e73bef50a8e42e2
b1f6c1c4 <b1f6c1c4@gmail.com>
2021-12-19 18:24:00 -0500

    [ci skip] copyright header

--- nginx/src/app/app.js
+++ nginx/src/app/app.js
@@ -1,3 +1,21 @@
+/* Copyright (C) 2020-2021 b1f6c1c4
+ *
+ * This file is part of ProfessionalAccounting.
+ *
+ * ProfessionalAccounting is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, version 3.
+ *
+ * ProfessionalAccounting is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with ProfessionalAccounting.  If not, see
+ * <https://www.gnu.org/licenses/>.
+ */
+
 import Edit from '../containers/edit/edit.js';
 
 export default function App(p, store) {
--- nginx/src/app/store.js
+++ nginx/src/app/store.js
@@ -1,3 +1,21 @@
+/* Copyright (C) 2020-2021 Iori Oikawa
+ *
+ * This file is part of ProfessionalAccounting.
+ *
+ * ProfessionalAccounting is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, version 3.
+ *
+ * ProfessionalAccounting is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with ProfessionalAccounting.  If not, see
+ * <https://www.gnu.org/licenses/>.
+ */
+
 import { configureStore } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
 import logger from 'redux-logger';
