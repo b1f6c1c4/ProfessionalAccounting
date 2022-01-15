ab4b2afd8e3c713ed18dde12fb4ec2be6f709e3a
IoriOikawa <iorioikawa@yahoo.co.jp>
2022-01-14 21:15:24 -0500

    Copyright Header updated

--- nginx/js/notice.js
+++ nginx/js/notice.js
@@ -18,7 +18,7 @@
 
 const notice = `ProfessionalAccounting
 Copyright (C) 2020-2022 b1f6c1c4
-Copyright (C) 2021 Iori Oikawa
+Copyright (C) 2021-2022 Iori Oikawa
 This is free software, licensed under AGPL-v3.0-only.
 See the source code for copying conditions.
 
