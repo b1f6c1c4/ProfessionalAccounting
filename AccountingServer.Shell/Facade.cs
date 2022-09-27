ebe4611f690d1a59b64111a9159d53c1248afd48
b1f6c1c4 <b1f6c1c4@gmail.com>
2022-09-26 22:00:35 -0400

    comments for QuadratureNormalization

--- AccountingServer.Shell/Facade.cs
+++ AccountingServer.Shell/Facade.cs
@@ -211,7 +211,12 @@ public class Facade
                 unc.Fund = -grpC.Sum(static d => d.Fund ?? 0D);
         }
 
-        if (grpCs.Count == 1 && grpUs.Count > 1)
+        // Automatically append T3998 and T3999 entries
+        if (grpCs.Count == 1 && grpUs.Count == 1) // single current, single users: nothing
+        {
+            // do nothing
+        }
+        else if (grpCs.Count == 1 && grpUs.Count > 1) // single current, multiple users: T3998
             foreach (var grpU in grpUs)
             {
                 var sum = grpU.Sum(static d => d.Fund!.Value);
@@ -221,7 +226,7 @@ public class Facade
                 voucher.Details.Add(
                     new() { User = grpU.Key, Currency = grpCs.First().Key, Title = 3998, Fund = -sum });
             }
-        else if (grpCs.Count > 1 && grpUs.Count == 1)
+        else if (grpCs.Count > 1 && grpUs.Count == 1) // single user, multiple currencies: T3999
             foreach (var grpC in grpCs)
             {
                 var sum = grpC.Sum(static d => d.Fund!.Value);
@@ -231,21 +236,26 @@ public class Facade
                 voucher.Details.Add(
                     new() { User = grpUs.First().Key, Currency = grpC.Key, Title = 3999, Fund = -sum });
             }
-        else
+        else // multiple user, multiple currencies: T3998 on payer, T3998/T3999 on payee
         {
             var grpUCs = voucher.Details.GroupBy(d => (d.User ?? session.Client.User, d.Currency ?? BaseCurrency.Now))
                 .ToList();
+            // list of payees (debit)
             var ps = grpUCs.Where(static grp => !grp.Sum(static d => d.Fund!.Value).IsNonPositive()).ToList();
+            // list of payers (credit)
             var qs = grpUCs.Where(static grp => !grp.Sum(static d => d.Fund!.Value).IsNonNegative()).ToList();
+            // total money received by payees (note: not w.r.t. to currency)
             var pss = ps.Sum(static grp => grp.Sum(static d => d.Fund!.Value));
+            // total money sent by payers (note: not w.r.t. to currency)
             var qss = qs.Sum(static grp => grp.Sum(static d => d.Fund!.Value));
             var lst = new List<VoucherDetail>();
-            if (ps.Count == 1 && qs.Count >= 1)
+            if (ps.Count == 1 && qs.Count >= 1) // one payee, multiple payers
                 foreach (var q in qs)
                     QuadratureNormalization(lst, ps[0], pss, q, qss);
-            else if (ps.Count >= 1 && qs.Count == 1)
+            else if (ps.Count >= 1 && qs.Count == 1) // multiple payees, one payer
                 foreach (var p in ps)
                     QuadratureNormalization(lst, p, pss, qs[0], qss);
+            // For multiple payee + multiple payer case, simply give up
             voucher.Details.AddRange(lst.GroupBy(static d => (d.User, d.Currency, d.Title), static (key, res)
                 => new VoucherDetail
                     {
@@ -259,20 +269,28 @@ public class Facade
         return session.Serializer.PresentVoucher(voucher).Wrap();
     }
 
+    /// <summary>
+    ///     When a user (q) pays another user (p) in different money
+    /// </summary>
+    /// <param name="details">Target</param>
+    /// <param name="p">The payee, user and currency</param>
+    /// <param name="pss">Total money received by payees</param>
+    /// <param name="q">The payer, user and currency</param>
+    /// <param name="qss">Total money sent by payers</param>
     private static void QuadratureNormalization(ICollection<VoucherDetail> details,
         IGrouping<(string, string), VoucherDetail> p, double pss,
         IGrouping<(string, string), VoucherDetail> q, double qss)
     {
-        var ps0 = p.Sum(static d => d.Fund!.Value);
-        var qs0 = q.Sum(static d => d.Fund!.Value);
-        var ps = ps0 * qs0 / qss;
-        var qs = qs0 * ps0 / pss;
-        // UpCp -> UqCp -> UqCq
+        var ps0 = p.Sum(static d => d.Fund!.Value); // money received by the payee
+        var qs0 = q.Sum(static d => d.Fund!.Value); // money sent by the payer
+        var ps = ps0 * qs0 / qss; // money received by the payee from that payer, in payee's currency
+        var qs = qs0 * ps0 / pss; // money sent by the payer to that payee, in payer's currency
+        // UpCp <- UqCp <- UqCq
         details.Add(new() { User = p.Key.Item1, Currency = p.Key.Item2, Title = 3998, Fund = -ps / 2 });
         details.Add(new() { User = q.Key.Item1, Currency = p.Key.Item2, Title = 3998, Fund = +ps / 2 });
         details.Add(new() { User = q.Key.Item1, Currency = p.Key.Item2, Title = 3999, Fund = -ps / 2 });
         details.Add(new() { User = q.Key.Item1, Currency = q.Key.Item2, Title = 3999, Fund = -qs / 2 });
-        // UpCp -> UpCq -> UqCq
+        // UpCp <- UpCq <- UqCq
         details.Add(new() { User = p.Key.Item1, Currency = p.Key.Item2, Title = 3999, Fund = -ps / 2 });
         details.Add(new() { User = p.Key.Item1, Currency = q.Key.Item2, Title = 3999, Fund = -qs / 2 });
         details.Add(new() { User = p.Key.Item1, Currency = q.Key.Item2, Title = 3998, Fund = +qs / 2 });
